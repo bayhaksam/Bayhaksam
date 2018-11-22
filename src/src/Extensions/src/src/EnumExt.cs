ac5e6503ede71f6fac4e145edf55e519af70330d
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-11-23 01:24:47 +0300

    Added enum flag extensions

---
+++ src/src/Extensions/src/src/EnumExt.cs
@@ -0,0 +1,43 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="EnumExt.cs" company="Bayhaksam">
+//      Copyright (c) Bayhaksam. All rights reserved.
+// </copyright>
+// <author>Samet Kurumahmut</author>
+//-----------------------------------------------------------------------
+
+namespace Bayhaksam.Extensions
+{
+	using System;
+
+	/// <summary>
+	/// <see cref="Enum"/>' s extension class
+	/// </summary>
+	public static class EnumExt
+	{
+		public static bool HasAnyFlag(this Enum source, params Enum[] flags)
+		{
+			foreach (var flag in flags)
+			{
+				if (source.HasFlag(flag))
+				{
+					return true;
+				}
+			}
+
+			return true;
+		}
+
+		public static bool HasFlags(this Enum source, params Enum[] flags)
+		{
+			foreach (var flag in flags)
+			{
+				if (!source.HasFlag(flag))
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+	}
+}
