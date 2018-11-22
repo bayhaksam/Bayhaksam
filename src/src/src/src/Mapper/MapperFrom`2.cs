317dbb79e074e05aa8beb3d1b362926d467ce800
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-11-22 23:46:20 +0300

    Added 'MapperFrom' abstraction

---
+++ src/src/src/src/Mapper/MapperFrom`2.cs
@@ -0,0 +1,14 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="MapperFrom`2.cs" company="Bayhaksam">
+//      Copyright (c) Bayhaksam. All rights reserved.
+// </copyright>
+// <author>Samet Kurumahmut</author>
+//-----------------------------------------------------------------------
+
+namespace Bayhaksam.Mapper
+{
+    public interface MapperFrom<in TFrom, out TTo>
+    {
+        TTo From(TFrom type);
+    }
+}
