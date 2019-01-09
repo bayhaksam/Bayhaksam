c35c81ddf3dd00a604cb6ca9b6f7a56613dbc254
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2019-01-09 16:03:26 +0300

    Added 'IMuter' abstraction to mute/un-mute sound

---
+++ src/src/src/src/Audio/IMuter.cs
@@ -0,0 +1,30 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="IMuter.cs" company="Bayhaksam">
+//      Copyright (c) Bayhaksam. All rights reserved.
+// </copyright>
+// <author>Samet Kurumahmut</author>
+//-----------------------------------------------------------------------
+
+namespace Bayhaksam.Audio
+{
+	/// <summary>
+	/// Muter interface to mute/un-mute the sound.
+	/// </summary>
+	public interface IMuter
+	{
+		/// <summary>
+		/// Mutes the sound.
+		/// </summary>
+		void Mute();
+
+		/// <summary>
+		/// Mutes the sound by specified <paramref name="value"/>.
+		/// </summary>
+		void Mute(bool value);
+
+		/// <summary>
+		/// Un-Mutes the sound.
+		/// </summary>
+		void UnMute();
+	}
+}
