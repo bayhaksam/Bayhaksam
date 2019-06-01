78bcd08f07d177a15326f76431ce3ac08c47112b
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2019-06-01 17:57:27 +0300

    Adds 'IsSameXXX' extensions method for 'DateTime'

--- src/src/Extensions/src/src/DateTimeExt.cs
+++ src/src/Extensions/src/src/DateTimeExt.cs
@@ -14,6 +14,44 @@ namespace Bayhaksam.Extensions
 	/// </summary>
 	public static class DateTimeExt
 	{
+		public static bool IsSameDay(this DateTime source, DateTime value) =>
+			source.Year == value.Year && source.Month == value.Month && source.Day == value.Day;
+
+		public static bool IsSameHour(this DateTime source, DateTime value) =>
+			source.Year == value.Year
+				&& source.Month == value.Month
+				&& source.Day == value.Day
+				&& source.Hour == value.Hour;
+
+		public static bool IsSameMillisecond(this DateTime source, DateTime value) =>
+			source.Year == value.Year
+				&& source.Month == value.Month
+				&& source.Day == value.Day
+				&& source.Hour == value.Hour
+				&& source.Minute == value.Minute
+				&& source.Second == value.Second
+				&& source.Millisecond == value.Millisecond;
+
+		public static bool IsSameMinute(this DateTime source, DateTime value) =>
+			source.Year == value.Year
+				&& source.Month == value.Month
+				&& source.Day == value.Day
+				&& source.Hour == value.Hour
+				&& source.Minute == value.Minute;
+
+		public static bool IsSameMonth(this DateTime source, DateTime value) =>
+			source.Year == value.Year && source.Month == value.Month;
+
+		public static bool IsSameSecond(this DateTime source, DateTime value) =>
+			source.Year == value.Year
+				&& source.Month == value.Month
+				&& source.Day == value.Day
+				&& source.Hour == value.Hour
+				&& source.Minute == value.Minute
+				&& source.Second == value.Second;
+
+		public static bool IsSameYear(this DateTime source, DateTime value) => source.Year == value.Year;
+
 		public static string ToLongTimeStampString(this DateTime source) => source.ToString("yyyyMMddHHmmssfffffff");
 	}
 }
