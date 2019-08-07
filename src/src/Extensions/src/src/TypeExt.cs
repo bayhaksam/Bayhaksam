f05b44afce39537f6ebfe002e5d988220342a230
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2019-08-07 14:49:31 +0300

    Added type extensions

---
+++ src/src/Extensions/src/src/TypeExt.cs
@@ -0,0 +1,25 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="TypeExt.cs" company="Bayhaksam">
+//      Copyright (c) Bayhaksam. All rights reserved.
+// </copyright>
+// <author>Samet Kurumahmut</author>
+//-----------------------------------------------------------------------
+
+namespace Bayhaksam.Extensions
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+
+	/// <summary>
+	/// <see cref="Type"/>' s extension class
+	/// </summary>
+	public static class TypeExt
+	{
+		public static IEnumerable<Type> GetConcreteTypes(this Type type) =>
+			type.Assembly.GetTypes().Where(x => !x.IsAbstract);
+
+		public static IEnumerable<Type> GetConcreteTypesSubclassOf(this Type type) =>
+			type.Assembly.GetTypes().Where(x => !x.IsAbstract && x.IsSubclassOf(type));
+	}
+}
